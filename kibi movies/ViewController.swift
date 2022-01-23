b551534c860604824a84e6f45b469020d9d160e1
andrianm28 <andrianmaulana28@gmail.com>
2022-01-23 14:50:01 +0700

    Initial Commit

---
+++ kibi movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kibi movies
+//
+//  Created by M. Andrian Maulana on 23/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
